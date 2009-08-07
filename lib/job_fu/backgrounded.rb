f6682e9d13d2052f4ca5039185a6f662422e460f
Jon Stenqvist <jon.stenqvist@gmail.com>
2009-08-07 16:01:38 +0200

    Added priority and process at to backgrounded handler

--- lib/job_fu/backgrounded.rb
+++ lib/job_fu/backgrounded.rb
@@ -2,7 +2,9 @@ module JobFu
   module Backgrounded
     class Handler
       def request(object, method, *args)
-        Job.enqueue ProcessableMethod.new(object, method, *args)
+        opt = args.extract_options!
+        priority, process_at = opt[:priority], opt[:at]
+        Job.enqueue ProcessableMethod.new(object, method, *args), priority, process_at
       end      
     end    
   end
--- spec/job_fu/backgrounded_spec.rb
+++ spec/job_fu/backgrounded_spec.rb
@@ -16,4 +16,15 @@ describe JobFu::Backgrounded::Handler do
     }.to change { JobFu::Job.count }
   end
   
+  it "should add with priority" do
+    described_class.new.request(MyBackgrounded.new, :do_stuff, :priority => 5)
+    JobFu::Job.last.priority.should == 5        
+  end
+  
+  it "should add with run at" do
+    process_at = 1.minute.from_now
+    described_class.new.request(MyBackgrounded.new, :do_stuff, :at => process_at)
+    JobFu::Job.last.process_at.should be_close(process_at, 1)
+  end
+  
 end
